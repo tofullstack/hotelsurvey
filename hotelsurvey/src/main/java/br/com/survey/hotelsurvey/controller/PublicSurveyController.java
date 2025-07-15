2db0a8102dd521dcf3b796e8a6778b3680bea9a5
tofullstack <caroleaveiro@gmail.com>
2025-07-15 10:11:38 -0300

    branch hotel survey desb

--- hotelsurvey/src/main/java/br/com/survey/hotelsurvey/controller/PublicSurveyController.java
+++ hotelsurvey/src/main/java/br/com/survey/hotelsurvey/controller/PublicSurveyController.java
@@ -1,13 +1,14 @@
 package br.com.survey.hotelsurvey.controller;
 
 import br.com.survey.hotelsurvey.dto.PublicSurveySectionDto;
+import br.com.survey.hotelsurvey.dto.SurveyResponseRequest;
 import br.com.survey.hotelsurvey.service.PublicApiService;
+import br.com.survey.hotelsurvey.service.SurveyResponseService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,11 +19,30 @@ public class PublicSurveyController {
     @Autowired
     private PublicApiService publicApiService;
 
+    @Autowired
+    private SurveyResponseService surveyResponseService;
+
     @GetMapping("/questions/{companyId}/{language}")
     public ResponseEntity<List<PublicSurveySectionDto>> getSurveyQuestions(
             @PathVariable Long companyId,
             @PathVariable String language) {
             List<PublicSurveySectionDto> surveyQuestions = publicApiService.getSurveyQuestions(companyId, language);
         return ResponseEntity.ok(surveyQuestions);
+    }
+
+    /*endpoint que recebe a resposta do hospede, nao precisa de autenticação
+    *@param request DTO contendo as respostas do formulário.
+    *@return ResponseEntity com o ID da resposta salva ou erro*/
+    @PostMapping("/submit-response")
+    public ResponseEntity<String> submitSurveyResponse(@Valid @RequestBody SurveyResponseRequest request) {
+        try {
+            Long responseId = surveyResponseService.submitSurveyResponse(request);
+            return new ResponseEntity<>("Survey response submitted successfully with ID: " + responseId, HttpStatus.CREATED);
+        } catch (Exception e) {
+            // captura exceções gerais para retornar uma mensagem de erro mais atraente
+            // as exceções especificas (ValidationException, ResourceNotFoundException)
+            // serão tratadas pelo Spring com os Status HTTP corretos (@ResponseStatus).
+            return new ResponseEntity<>("Error submitting survey response: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
